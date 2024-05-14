062f9bfc08c4b548093650d6bde9cb829dd5dc77
groot <sasirath77@gmail.com>
2024-05-14 12:59:47 +0530

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+Route::resource('vendor', App\Http\Controllers\VendorController::class);
+Route::resource('user', App\Http\Controllers\CustomersController::class);
+Route::resource('zone', App\Http\Controllers\ZoneController::class);
