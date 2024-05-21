80ebaefb633d7cbc11fb869ca183c920079d102b
josh <joshuva5674@gmail.com>
2024-05-21 17:49:06 +0530

    layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    ThemePark
                 </a>
 
                 <a class="navbar-brand" href="{{ url('/vendor') }}">
