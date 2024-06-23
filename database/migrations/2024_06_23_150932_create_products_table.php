9dbf31f4d9fa745a6cabd861a67c86c1b9342a9b
groot <sasirath77@gmail.com>
2024-06-23 23:05:05 +0530

    Product added

--- database/migrations/2024_06_23_150932_create_products_table.php
+++ database/migrations/2024_06_23_150932_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('vendor_id')->constrained()->onDelete('cascade');
+            $table->string('vendor_id');
             $table->string('product_name');
             $table->string('description');
             $table->decimal('price', 8, 2);
